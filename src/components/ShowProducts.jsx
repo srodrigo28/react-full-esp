f3c3f3d915b76a3bb5c83e729bc798c3b0d40893
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-09-23 18:45:04 -0300

    updated project

--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -1,9 +1,43 @@
 import { useEffect, useState } from "react"
 
 export function ShowProducts(){
+    const url = "";
+
+    const [products, setProducts] = useState([]);
+    const [id, setId] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [operation, setOperation] = useState(1);
+    const [title, setTitle] = useState('');
+
+    const getProducts = async () => {
+        const repuesta = await axios.get(url);
+        setProducts(repuesta.data);
+    }
+
+    useEffect( () => {
+        getProducts();
+    })
+
     return(
         <div>
-            <h1>ShowProducts</h1>
+            <h1>{title}</h1>
+            <div className="container-fluid">
+                <div className="row mt-3">
+                    <div className="col-md-4 offset-md-4">
+                        <div className="d-grid mx-auto">
+                            <button className="btn btn-dark" data-bs-toggle='modal' data-bs-target="#modalProducts">
+                                <i className="fa-solid fa-circle-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="modal fade">
+                
+            </div>
         </div>
     )
 }
